4d69e94dc45284e0eba3c74ab0360b790ced586c
Радченко Катерина <ketrinradchenko@gmail.com>
2023-06-05 17:57:52 +0300

    туц

--- task1/Program.cs
+++ task1/Program.cs
@@ -40,10 +40,10 @@ namespace task1
                 switch (choice)
                 {
                     case 1:
-                        obj.AddOnePerson();
+                        obj.AddPerson();
                         break;
                     case 2:
-                        obj.DeleteOnePerson();
+                        obj.DeletePerson();
                         break;
                     case 3:
                         obj.ChangeInfo();
@@ -58,86 +58,90 @@ namespace task1
             } while (choice!=6);
         }
     }
-    class User
+    class User  // класс Пользователь
     {
-        public string Login { get; set; }
-        public string Password { get; set; }
+        public string Login { get; set; }     // свойство логин
+        public string Password { get; set; }  // свойство пароль
     }
-    class Logins : User
+    class Logins : User  // класс Логин, наследует класс Пользователь
     {
+        // создаем словарь с ключам типа string, значениями типа string
         public static Dictionary<string, string> MyDict = new Dictionary<string, string>();
         
-        public void AddOnePerson()
+        public void AddPerson()  // метод для добавления логина и пароля
         {
-            try
+            try  // здесь может быть сгенерировано исключение
             {
                 Console.WriteLine("Введите логин: ");
-                Login = Console.ReadLine();
+                Login = Console.ReadLine();     // считываем введенный юзером логин
                 Console.WriteLine("Введите пароль: ");
-                Password = Console.ReadLine();
-                if (Password.Length < 4)
-                {
+                Password = Console.ReadLine();  // считываем введенный юзером пароль
+                if (Password.Length < 4)        // если пароль содержит менее 4 символов
+                {                               // то выбрасываем исключение
                     throw new Exception("Пароль должен содержать минимум 4 символа");
                 }
             }
-            catch(Exception ex) { Console.WriteLine(ex.Message); }
-           
-            try
+            catch(Exception ex)                 // отлов ичключения
+            { 
+                Console.WriteLine(ex.Message);  // вывод информации об исключении
+            }
+
+            try  // здесь может быть сгенерировано исключение
             {
-                MyDict.Add(Login, Password);
+                MyDict.Add(Login, Password);  // добавление логина и пароля в словарь
                 Console.WriteLine($"Логин {Login} и пароль к нему добавлены!");
             }
-            catch (ArgumentException)
+            catch (ArgumentException)         // отлов исключения, если такой ключ уже есть в словаре
             {
                 Console.WriteLine($"Елемент с таким логином {Login} уже существует!");
             }           
         }
 
-        public void DeleteOnePerson()
+        public void DeletePerson()  // метод для удаления логина и пароль
         {
             Console.WriteLine("Введите логин для удаления: ");
-            Login = Console.ReadLine();
-            if(MyDict.ContainsKey(Login))
+            Login = Console.ReadLine();     // считываем введенный юзером логин
+            if (MyDict.ContainsKey(Login))  // если словарь содержит такой ключ(логин)
             {
-                MyDict.Remove(Login);
+                MyDict.Remove(Login);       // то удаляем логин из словаря
                 Console.WriteLine($"Логин {Login} и пароль к нему удалены!");
             }
-            else if (!MyDict.ContainsKey(Login))
+            else if (!MyDict.ContainsKey(Login))  // иначе, если словарь не содержит такой логин
             {
                 Console.WriteLine($"Логин {Login} не существует!");
             }          
         }
         
-        public void WatchPassword()
+        public void WatchPassword()  // метод для просмотра пароля по логину
         {
             Console.WriteLine("Enter login to watch password: ");
-            Login = Console.ReadLine();
-            Console.WriteLine($"Для логина {Login} пароль: {MyDict[Login]}");
+            Login = Console.ReadLine();       // считываем введенный юзером логин
+            Console.WriteLine($"Для логина {Login} " +
+                $"пароль: {MyDict[Login]}");  // выводи в консоль пароль по ключу
         }
 
-        public void ChangeInfo()
+        public void ChangeInfo()  // метод для изменения пароля
         {
             Console.WriteLine("Ведите логин, по которому вы хотите изменить пароль: ");
-            Login = Console.ReadLine();
+            Login = Console.ReadLine();     // считываем введенный юзером логин
             Console.WriteLine("Введите новый пароль: ");
-            Password = Console.ReadLine();
-            //MyDict[Login] = Password;
-            if (MyDict.ContainsKey(Login))
+            Password = Console.ReadLine();  // считываем введенный новый пароль
+            if (MyDict.ContainsKey(Login))  // если словарь содержит такой логин
             {
-                MyDict[Login] = Password;
+                MyDict[Login] = Password;   // то меняем пароль по ключу
                 Console.WriteLine($"Логин {Login} и пароль к нему изменены!");
             }
-            else if (!MyDict.ContainsKey(Login))
+            else if (!MyDict.ContainsKey(Login))  // иначе, если такой логин не существует
             {
                 Console.WriteLine($"Логин {Login} не существует!");
             }          
         }
 
-        public void ShowInfo()
+        public void ShowInfo()  // метод для вывода информации в консоль
         {
-            foreach (KeyValuePair<string, string> pair in MyDict)
+            foreach (KeyValuePair<string, string> pair in MyDict)  // цикл по парам в словаре
             {
-                Console.WriteLine(pair.Key + " - " + pair.Value);
+                Console.WriteLine(pair.Key + " - " + pair.Value);  // выводим ключ и его значение
             }
         }
     }
--- task2/Program.cs
+++ task2/Program.cs
@@ -75,196 +75,191 @@
         }
     }
    
-    class RusList 
+    class RuList  // класс Список переводов
     {
-        public List<string> MyList; 
+        public List<string> MyList;
     
-        public List<string> AddRusVar()
+        public List<string> AddRusVar()   // метод для добавления перевода в список
         {
-            MyList = new List<string>();
-            bool c = true;
+            MyList = new List<string>();  // выделяем память под список
+            bool c = true;                // переменная для цикла
             do
             {
                 Console.Write("Добавить новый перевод - нажмите 1, нет - нажмте 0");
                 int user_choice = 0;
-                try
+                try                       // может быть сгенерировано исключение неверного ввода пункта выбора
                 {
                     user_choice = Int32.Parse(Console.ReadLine());
                 }
-                catch(FormatException)
+                catch(FormatException)    // если введена не цифра - генерируем FormatException
                 {
                     Console.WriteLine("FormatException");
                 }
                 
-                switch(user_choice)
+                switch(user_choice)       // в зависимости от выбора пользователя
                 {
-                    case 1:
+                    case 1:               // если юзер нажал 1:
                         Console.Write("Введите вариант перевода на русском:");
-                        string str = Console.ReadLine();
-                        MyList.Add(str);
+                        string str = Console.ReadLine();  // считываем введенную строку
+                        MyList.Add(str);                  // добавить строку к списку перевода
                         break;
-                    case 0:
+                    case 0:               // если юзер наджал 0 - выход из цикла
                         c = false;
                         break;
                     default:
                         Console.WriteLine("Invalid choice!");
                         break;
-                }
-               
+                }              
             } while (c);
             return MyList;
         }
     }
-    class EnRusDictionary : RusList
+    class EnRusDictionary : RuList  // класс Словарь, наследует класс Список переводов
     {
         public static Dictionary<string, List<string>> MyDict 
             = new Dictionary<string, List<string>>();
     
-        public void AddDict()
+        public void AddDict()  // метод для добавления слова в словарь
         {
             Console.Write("Введите слово на английском:");
-            string str = Console.ReadLine();
+            string str = Console.ReadLine();   // считываем введенной слово
 
-            if (MyDict.ContainsKey(str))
+            if (MyDict.ContainsKey(str))       // если слово уже есть в словаре  -выводим информацию об этом
                 Console.WriteLine("Такое слово уже есть в словаре");
-            else
-                MyDict.Add(str, AddRusVar());
+            else                               // если такого слова нет
+                MyDict.Add(str, AddRusVar());  // добавляем слово как ключ, как значение - вызов метода AddRusVar()
         }
 
-        public void AddTranslate()
+        public void AddTranslate()  // метод для добавления перевода к уже существующему слову
         {
             Console.Write("Введите слово на английском, для которого вы хотите добавить перевод:");
-            string str_en = Console.ReadLine();
-            if (!MyDict.ContainsKey(str_en))
+            string str_en = Console.ReadLine();  // считываем введенной слово
+            if (!MyDict.ContainsKey(str_en))     // если такого слова нет - выводим информацию и выходим из метода
             {
                 Console.WriteLine($"слово не найдено");
                 return;
             }
-
             Console.Write("Какой русскоий перевод добавить?");           
-            string str_rus = Console.ReadLine();
-            if (MyList.Contains(str_rus))
+            string str_rus = Console.ReadLine();  // считываем введенный перевод
+            if (MyList.Contains(str_rus))         // если такой перевод уже есть - выводим информацию
                 Console.WriteLine("Такой вариант перевода уже есть.");
-            else if (MyDict.ContainsKey(str_en))
+            else if (MyDict.ContainsKey(str_en))  // иначе, если такого перевода нет
             {
-                MyDict[str_en].Add(str_rus);
-                //Dict[str] = AddRusVar();
+                MyDict[str_en].Add(str_rus);      // добавляем перевод по ключу
             }
         }
 
-        public void DeleteDict()
+        public void DeleteDict()  // метод для удаления слова
         {
             Console.Write("Введите слово на английском, которое выходить удалить:");
-            string str = Console.ReadLine();
-            if (!MyDict.ContainsKey(str))
+            string str = Console.ReadLine();  // считываем введенное слово
+            if (!MyDict.ContainsKey(str))     // если такого слова нет - выводим информацию и выходим из метода
             {
                 Console.WriteLine($"слово не найдено");
                 return;
             }
-            else
+            else                     // иначе, если слово есть
             {
-                MyDict.Remove(str);
+                MyDict.Remove(str);  // удаляем слово из словаря
                 Console.WriteLine($"Слово {str} и его переводы удалено.");
             }            
         }
 
-        public void DeleteTranslate()
+        public void DeleteTranslate()  // метод для удаления перевода
         {
             Console.Write("Введите слово на английском, для которого вы хотите удалить перевод:");
-            string str_en = Console.ReadLine();
-            if (!MyDict.ContainsKey(str_en))
+            string str_en = Console.ReadLine();  // считываем введенной слово
+            if (!MyDict.ContainsKey(str_en))     // если такого слова нет - выводим информацию и выходим из метода
             {
                 Console.WriteLine($"слово не найдено");
                 return;
             }
 
             Console.Write("Какой русскоий перевод вы хотите удалить?");
-            string str_rus = Console.ReadLine();
-            if (!MyList.Contains(str_rus))
+            string str_rus = Console.ReadLine();  // считываем введенный перевод
+            if (!MyList.Contains(str_rus))        // если такого перевода нет  - выводим информацию
                 Console.WriteLine("Такой вариант перевода не найден.");
-            else if(MyList.Contains(str_rus))
+            else if(MyList.Contains(str_rus))     // если такой перевод есть
             {
-                if (MyList.Count < 2)
-                {
+                if (MyList.Count < 2)             // если в списке только один перевод
+                {                                 // то запрашиваем, что в случае удаления последнего перевода удаляется слово
                     Console.WriteLine("В списке переводо одно слово. Если удалить его - будет удалено и слово." +
                         "Удалить последний перевод и слово? Если да - нажмите 1, если нет - нажмите 0.");
                     int c = Int32.Parse(Console.ReadLine());
-                    if (c == 1)
+                    if (c == 1)                   // если юзер хочет удалтьь последний перевол и все слово
                     {
-                       // MyList.Remove(str_rus);
-                        MyDict.Remove(str_en);
+                        MyDict.Remove(str_en);    // удаляем слово из словаря
                         Console.WriteLine($"Слово {str_en} и перевод удалено.");
                     }
                 }
-                else
-                    MyList.Remove(str_rus);
+                else                              // иначе, если удаляемый перевод не последний
+                    MyList.Remove(str_rus);       // удаляем перевод из листа
                 Console.WriteLine($"Перевод {str_rus} удален.");
             }
         }
 
-        public void ChangeDict()
+        public void ChangeDict()  // метод для изменения слова
         {
             Console.Write("Введите слово на английском, котрое вы хотите изменить:");
-            string str_en = Console.ReadLine();
-            if (!MyDict.ContainsKey(str_en))
+            string str_en = Console.ReadLine();  // считываем введенное слово
+            if (!MyDict.ContainsKey(str_en))     // если такого слова нет - выводи инфо
                 Console.WriteLine("Такого слове нет в словаре.");
-            else
+            else                                 // если введенное слово есть в словаре
             {
                 Console.WriteLine("Введите новое слово:");
-                string str_en2 = Console.ReadLine();
-                List<string> content = new List<string>();
-                content = MyDict[str_en];
-                MyDict.Remove(str_en);
-                MyDict.Add(str_en2, content);
-                //MyDict[str_en] = MyDict[str_en2];
+                string str_en2 = Console.ReadLine();        // считываем введенное слово для изменения
+                List<string> content = new List<string>();  // создаем новый список
+                content = MyDict[str_en];                   // в новый список записываем переводды из старого списка
+                MyDict.Remove(str_en);                      // удаляем старое слово
+                MyDict.Add(str_en2, content);               // добавляем новое слово со старыми переводами
                 Console.WriteLine($"Слово {str_en} изменилось на {str_en2}.");
             }
         }
 
-        public void ChangeTranslate()
+        public void ChangeTranslate()  // метод для изменения перевода
         {
             Console.Write("Введите слово на английском, для которого вы хотите изменить вариант перевода:");
-            string str_en = Console.ReadLine();
-            if (!MyDict.ContainsKey(str_en))
+            string str_en = Console.ReadLine();  // считываем введенное слово
+            if (!MyDict.ContainsKey(str_en))     // если нет в словаре - выводи инфо и выходим из метода
             {
                 Console.WriteLine("Такого слове нет в словаре.");
                 return;
             }
             Console.Write("Какой русскоий перевод вы хотите изменить?");
-            string str_rus_old = Console.ReadLine();
-            if (!MyList.Contains(str_rus_old))
+            string str_rus_old = Console.ReadLine();  // считываем введенный старый перевод
+            if (!MyList.Contains(str_rus_old))        // если такого перевода нет - выводим инфо
                 Console.WriteLine("Такой вариант перевода не найден.");
-            else if(MyList.Contains(str_rus_old))
+            else if(MyList.Contains(str_rus_old))     // если перевод есть
             {
                 Console.WriteLine("Введите новый перевод:");
-                string str_rus_new = Console.ReadLine();
-                int index = MyList.IndexOf(str_rus_old);
-                MyList.Remove(str_rus_old);
-                MyList.Insert(index, str_rus_new);
+                string str_rus_new = Console.ReadLine();  // считываем введенный новый перевод
+                int index = MyList.IndexOf(str_rus_old);  // узнаем индекс этого перевода в списке
+                MyList.Remove(str_rus_old);               // удаляем старый перевод из списка
+                MyList.Insert(index, str_rus_new);        // втсавляем новый перевод в список на место удаленного
             }
         }
 
-        public void SearchTranslate()
+        public void SearchTranslate()  // метод для поиска перевода (программа выведет слово)
         {
-            bool c = false;
+            bool c = false;            // переменная для цикла
             Console.WriteLine("Введите перевод, который вы хотите найти");
-            string str_ru = Console.ReadLine();
+            string str_ru = Console.ReadLine();  // считываем введенный перевод
            
-            foreach (KeyValuePair<string, List<string>> kvp in MyDict)
+            foreach (KeyValuePair<string, List<string>> kvp in MyDict)  // цикл по словарю
             {             
-                if (kvp.Value.Contains(str_ru))
+                if (kvp.Value.Contains(str_ru))  // есть в словаре в значениях  есть такой перевод
                 {
                     c = true;
-                    Console.Write($"\nАнглийское слово:\n{kvp.Key}");
+                    Console.Write($"\nАнглийское слово:\n{kvp.Key}");  // то выводим ключ (слово) для этого значения
                 }                
             }
-            if (c == false)
+            if (c == false)  // иначе перевод не найден, выводим инфо
             {
                 Console.WriteLine("Нет такого перевода");
             }
         }
     
-        public void ShowDict()
+        public void ShowDict()  // метод для вывода словаря в консоль
         {
             foreach(KeyValuePair<string, List<string>> kvp in MyDict)
             {
